a36d1e358819092191d771b66f542ab231c82c88
Mauricio Paicil <mauricio.paicil@latam.com>
2018-02-06 11:04:17 -0300

    Se agrega nueva configuracion con Fanout Exchange, aunque aun no funciona como publicacion/suscriptor

---
+++ messagemq/readme.md
@@ -0,0 +1,5 @@
+Mesajeria MQ by REST
+
+Informacion de implementacion: 
+* https://spring.io/guides/gs/messaging-rabbitmq/
+* https://www.rabbitmq.com/tutorials/tutorial-one-spring-amqp.html
--- messagemq/src/main/java/com/slackmag/config/Config.java
+++ messagemq/src/main/java/com/slackmag/config/Config.java
@@ -3,8 +3,8 @@ package com.slackmag.config;
 import com.slackmag.domain.Receiver;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -14,30 +14,32 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Config {
 
-    public final static String queueName = "spring-boot";
+    public final static String queueName = "spring-boot4";
 
     @Bean
     Queue queue() {
-        return new Queue(queueName, false);
+        return new Queue(queueName, false,false,true);
     }
 
     @Bean
-    TopicExchange exchange() {
-        return new TopicExchange("spring-boot-exchange");
+    FanoutExchange exchange() {
+        return new FanoutExchange("spring-boot-exchange4");
     }
 
     @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    Binding binding(Queue queue, FanoutExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange);
     }
 
     @Bean
     SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
                                              MessageListenerAdapter listenerAdapter) {
+
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(queueName);
+        container.setQueues(queue());
         container.setMessageListener(listenerAdapter);
+
         return container;
     }
 
